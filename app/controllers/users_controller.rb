e6d236d2218c3d3d6246cebbb73d665d6f352e4b
smenon2003 <146893219+smenon2003@users.noreply.github.com>
2023-11-21 23:31:03 +0000

    adding nav bar and /users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action(:authenticate_user!, { :only => [:index] })
   def index
     matching_users = User.all
     @list_of_users = matching_users.order({ :created_at => :desc })
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "users", :action => "index"})
 
+  get("/users", { :controller => "users", :action => "index"})
+
   # Routes for the Like resource:
 
   # CREATE
