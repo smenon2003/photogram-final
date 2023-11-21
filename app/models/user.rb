3d06f675205864587e910fb8f999f615de7000f7
smenon2003 <146893219+smenon2003@users.noreply.github.com>
2023-11-21 23:04:55 +0000

    associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,13 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
+  has_many  :photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
+  has_many  :comments, class_name: "Comment", foreign_key: "photo_id", dependent: :destroy
+  has_many  :likes, class_name: "Like", foreign_key: "fan_id", dependent: :destroy
+  has_many  :sent_follow_requests, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
+  has_many  :received_follow_requests, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy
+  has_many :followings, through: :sent_follow_requests, source: :recipient
+  has_many :followers, through: :received_follow_requests, source: :sender
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
